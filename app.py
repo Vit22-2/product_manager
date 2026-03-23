e1b71394b2863f02cb201dc0c1cf7b376997dbc5
Shinto <vithourakk@gmail.com>
2026-03-23 11:07:59 +0700

    feat: fixed login() import issue

--- app.py
+++ app.py
@@ -1,12 +1,68 @@
 import os
 import datetime
 from cs50 import SQL
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, flash, render_template, redirect, session, request
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import usd, login_required
 
+#Configure application
 app = Flask(__name__)
 
+#Configure database to use CS50' SQLite
 db = SQL("sqlite:///finance.db")
-@app.route("/")
-def index():
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#Custom filter
+app.jinja_env.filters["usd"] = usd
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    session.clear()
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("must provide password")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            flash("invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
